3cc51b47443b913e58e3f635a303f5eeb2ab4637
George Apostolov <za.tebe.e@gmail.com>
2022-02-19 00:27:51 +0200

    Admin panel fix

--- smvc/controller/admin/login.php
+++ smvc/controller/admin/login.php
@@ -3,36 +3,35 @@
 		public function __construct() {
 			parent::__construct();
 		}
-		public function index() {
-			$result = array();
-			if (!empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'login') {
+		public function index() {
+			$result = array();
+			if (!empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'login') {
 				$user = new model_public_user();
 				$username = (isset($_POST['username']) && $_POST['username'] ? trim($_POST['username']) : '');
-				$password = (isset($_POST['password']) && $_POST['password'] ? trim($_POST['password']) : '');
+				$password = (isset($_POST['password']) && $_POST['password'] ? trim($_POST['password']) : '');
 				$result = $user->login($username, $password);
 				if ($result['success'] == 1) {
-					header('Location: ' . ROOT_URL . 'admin');
-				} else {
-					if (isset($_SESSION['2fa_enabled'])) {
+					header('Location: ' . ROOT_URL . 'admin');					return;
+				} else {
+					if (isset($_SESSION['tfa_enabled'])) {
 						header('Location: ' . ROOT_URL . 'admin/login/tfa');
 						return;
-					}					
-				}
-			}
+					}
+				}
+			}
 			$this->view->set('result', $result);
 			$this->view->set('title', 'admin login index');
-			$this->view->display('login/index');
+			$this->view->display('login/index');
 		}
-
 		public function tfa() {			
 			$result = [];			
-			if (!empty($_POST) && isset($_POST['action']) && $_POST['action'] == '2fa') {				
+			if (!empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'tfa') {				
 				$user = new model_public_user();
-				$username = (isset($_SESSION['2fa_enabled']) && $_SESSION['2fa_enabled'] ? $_SESSION['2fa_enabled'] : '');
-				$tfa_code = (isset($_POST['2fa']) && $_POST['2fa'] ? trim($_POST['2fa']) : '');				
+				$username = (isset($_SESSION['tfa_enabled']) && $_SESSION['tfa_enabled'] ? $_SESSION['tfa_enabled'] : '');
+				$tfa_code = (isset($_POST['tfa']) && $_POST['tfa'] ? trim($_POST['tfa']) : '');				
 				$result = $user->login($username, $tfa_code);
 				if ($result['success'] == 1) {
-					header('Location: ' . ROOT_URL . 'admin');
+					header('Location: ' . ROOT_URL . 'admin');					return;
 				}				
 			}			
 			$this->view->set('result', $result);
